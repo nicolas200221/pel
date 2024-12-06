08c6d89620f1dd2decc64f4737fbc3abb4e24eb4
nicolas bertoncello <nicolas165@Outlook.it>
2024-12-06 17:29:58 +0100

    implemented getters and setters, started implementation of trie<T>::add_child (see function comment)

--- progetto/src/trie.cpp
+++ progetto/src/trie.cpp
@@ -59,3 +59,31 @@ trie<T>& trie<T>::operator=(trie<T>&& rhs){
     rhs.m_c = bag<trie<T>>();
     rhs.m_w = 0;
 }
+
+template <typename T>
+void trie<T>::set_weight(double w){ this->m_w = w; }
+
+template <typename T>
+double trie<T>::get_weight() const { return this->m_w; }
+
+template <typename T>
+void trie<T>::set_label(T* l) { this->m_l = l; }
+
+template <typename T>
+T const* trie<T>::get_label() const { return this->m_l; }
+
+template <typename T>
+void trie<T>::set_parent(trie<T>* p) { this->m_p = p; }
+
+template <typename T>
+trie<T> const* trie<T>::get_parent() const { return this->m_p; }
+
+template <typename T>
+bag<trie<T>> const& trie<T>::get_children() const { return this->m_c; }
+
+//implementa anche prepend per l'ordinamento, oltre all'operator <;
+template <typename T>
+void trie<T>::add_child(trie<T> const& c) {
+    this->m_w = 0.0;
+
+}
