c22b7e4e7f17617dace9e6879beaf5713f538372
nicolas bertoncello <nicolas165@Outlook.it>
2024-12-06 13:21:41 +0100

    added and implemented all copy constructor

--- progetto/src/tools/test.cpp
+++ progetto/src/tools/test.cpp
@@ -5,14 +5,13 @@ int main(){
     bagchar.append('a');
     bagchar.append('b');
     bagchar.print();
-    bag<char> bagint;
-    bagint.append(15);
+    bag<int> bagint;
     bagint.append(15);
     bagint.append(20);
     bagint.append(25);
     bagint.append(30);
     bagint.append(35);
     bagint.print();
-    bag<char> bagintcopy(bagint);
+    bag<int> bagintcopy(bagint);
     return 0;
 }
--- progetto/src/trie.cpp
+++ progetto/src/trie.cpp
@@ -25,3 +25,37 @@ trie<T>::trie(trie<T> const& copy) {
         this = this->m_c;
     }
 }
+
+template <typename T>
+trie<T>::trie(trie<T>&& rhs){
+    m_l = rhs.m_l;
+    m_c = std::move(rhs.m_c);
+    m_w = rhs.m_w;
+
+    rhs.m_p = nullptr;
+    rhs.m_l = nullptr;
+    rhs.m_c = bag<trie<T>>();
+    rhs.m_w = 0;
+}
+
+template <typename T>
+trie<T>& trie<T>::operator=(trie<T> const& copy){
+    for(auto& x : copy.m_c) {
+        this->m_l = copy.m_l;
+        this->m_w = copy.m_w;
+        this->m_c = new bag<trie<T>>;
+        this = this->m_c;
+    }
+}
+
+template <typename T>
+trie<T>& trie<T>::operator=(trie<T>&& rhs){
+    m_l = rhs.m_l;
+    m_c = std::move(rhs.m_c);
+    m_w = rhs.m_w;
+
+    rhs.m_p = nullptr;
+    rhs.m_l = nullptr;
+    rhs.m_c = bag<trie<T>>();
+    rhs.m_w = 0;
+}
